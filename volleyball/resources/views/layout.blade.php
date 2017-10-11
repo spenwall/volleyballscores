f6e206102c2778f4b34860c9b76c35dc1db43957
spenwall <dude.wallace@gmail.com>
2017-10-10 23:29:53 -0600

    Some css changes

--- volleyball/resources/views/layout.blade.php
+++ volleyball/resources/views/layout.blade.php
@@ -5,9 +5,7 @@
   <meta charset="utf-8">
 
   <title>Medicine Hat Volleyball Scores</title>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
